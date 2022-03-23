dbd9ef7087ecbe9c79ffbda95c53c4701b0e3131
lack <598223084@qq.com>
2022-03-23 16:53:09 +0800

    maxsubarray2

--- datastruct/day1/max_sub_array.go
+++ datastruct/day1/max_sub_array.go
@@ -2,7 +2,6 @@ package day1
 
 // 贪心算法
 func MaxSubArray1(nums []int) int {
-
 	max := nums[0]
 	for i := 1; i < len(nums); i++ {
 		if nums[i]+nums[i-1] > nums[i] {
@@ -16,4 +15,36 @@ func MaxSubArray1(nums []int) int {
 	return max
 }
 
-// 动态规划
+// 分治发
+func MaxSubArray2(nums []int) int {
+	return get(nums, 0, len(nums)-1).mSum
+}
+
+func get(nums []int, l, r int) Status {
+	if l == r {
+		return Status{nums[l], nums[l], nums[l], nums[l]}
+	}
+	m := (l + r) >> 1
+	lSub := get(nums, l, m)
+	rSub := get(nums, m+1, r)
+	return pushUp(lSub, rSub)
+}
+
+func pushUp(l, r Status) Status {
+	iSum := l.iSum + r.iSum
+	lSum := max(l.lSum, l.iSum+r.lSum)
+	rSum := max(r.rSum, r.iSum+l.rSum)
+	mSum := max(max(l.mSum, r.mSum), l.rSum+r.lSum)
+	return Status{lSum, rSum, mSum, iSum}
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+type Status struct {
+	lSum, rSum, mSum, iSum int
+}
--- datastruct/day1/max_sub_array_test.go
+++ datastruct/day1/max_sub_array_test.go
@@ -20,7 +20,31 @@ func TestMaxSubArray1(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := MaxSubArray1(tt.args.nums); got != tt.want {
-				t.Errorf("MaxSubArray() = %v, want %v", got, tt.want)
+				t.Errorf("MaxSubArray1() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMaxSubArray2(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{name: "TestMaxSubArray2-1", args: struct{ nums []int }{nums: []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}}, want: 6},
+		{name: "TestMaxSubArray2-2", args: struct{ nums []int }{nums: []int{1}}, want: 1},
+		{name: "TestMaxSubArray2-3", args: struct{ nums []int }{nums: []int{5, 4, -1, 7, 8}}, want: 23},
+		{name: "TestMaxSubArray2-3", args: struct{ nums []int }{nums: []int{-1}}, want: -1},
+		{name: "TestMaxSubArray2-3", args: struct{ nums []int }{nums: []int{1, 2, -1, -2, 2, 1, -2, 1}}, want: 3},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := MaxSubArray2(tt.args.nums); got != tt.want {
+				t.Errorf("MaxSubArray2() = %v, want %v", got, tt.want)
 			}
 		})
 	}
