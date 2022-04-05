8ca28d991e9fd2053e7f0e184fd6f7fccc0faba3
lack <598223084@qq.com>
2022-04-05 20:13:33 +0800

    modify

--- offer01/day01/cqueue.go
+++ offer01/day01/cqueue.go
@@ -5,30 +5,30 @@ type CQueue struct {
 	tail []int
 }
 
-//func Constructor() CQueue {
-//	return CQueue{
-//		head: make([]int, 0),
-//		tail: make([]int, 0),
-//	}
-//}
-//
-//func (this *CQueue) AppendTail(value int) {
-//	this.tail = append(this.tail, value)
-//}
-//
-//func (this *CQueue) DeleteHead() int {
-//	if len(this.head) > 0 {
-//		v := this.head[len(this.head)-1]
-//		this.head = this.head[:len(this.head)-1]
-//		return v
-//	}
-//	if len(this.tail) > 0 {
-//		for i := len(this.tail) - 1; i >= 0; i-- {
-//			this.head = append(this.head, this.tail[i])
-//		}
-//		this.tail = make([]int, 0)
-//		return this.DeleteHead()
-//	}
-//
-//	return -1
-//}
+func Constructor() CQueue {
+	return CQueue{
+		head: make([]int, 0),
+		tail: make([]int, 0),
+	}
+}
+
+func (this *CQueue) AppendTail(value int) {
+	this.tail = append(this.tail, value)
+}
+
+func (this *CQueue) DeleteHead() int {
+	if len(this.head) > 0 {
+		v := this.head[len(this.head)-1]
+		this.head = this.head[:len(this.head)-1]
+		return v
+	}
+	if len(this.tail) > 0 {
+		for i := len(this.tail) - 1; i >= 0; i-- {
+			this.head = append(this.head, this.tail[i])
+		}
+		this.tail = make([]int, 0)
+		return this.DeleteHead()
+	}
+
+	return -1
+}
