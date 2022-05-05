815668b1727520ef50e701a8349066b15b45f9a8
lack <598223084@qq.com>
2022-05-05 18:20:07 +0800

    offer day26

--- offer01/day26/isNumber.go
+++ offer01/day26/isNumber.go
@@ -1,1 +1,43 @@
 package day26
+
+import "strings"
+
+func isNumber(s string) bool {
+	arr := strings.TrimSpace(s)
+	if len(arr) == 0 {
+		return false
+	}
+	index := 0
+	if arr[index] == '+' || arr[index] == '-' {
+		index++
+	}
+	cur := index
+	index = scanUnsignedInteger(arr, index)
+	numeric := index > cur
+	if index < len(arr) && arr[index] == '.' {
+		//略过小数点
+		index++
+		cur = index
+		index = scanUnsignedInteger(arr, index)
+		numeric = (index > cur) || numeric
+	}
+	if index < len(arr) && (arr[index] == 'e' || arr[index] == 'E') {
+		//略过e/E
+		index++
+		//略过符号位
+		if index < len(arr) && (arr[index] == '+' || arr[index] == '-') {
+			index++
+		}
+		cur = index
+		index = scanUnsignedInteger(arr, index)
+		numeric = (index > cur) && numeric
+	}
+	return numeric && index == len(arr)
+}
+
+func scanUnsignedInteger(arr string, index int) int {
+	for index < len(arr) && arr[index] >= '0' && arr[index] <= '9' {
+		index++
+	}
+	return index
+}
--- offer01/day26/strToInt.go
+++ offer01/day26/strToInt.go
@@ -1,1 +1,42 @@
 package day26
+
+import "math"
+
+func strToInt(str string) int {
+	index := 0
+	length := len(str)
+	if length == 0 {
+		return 0
+	}
+
+	syn := 1
+	res := 0
+	max, min := math.MaxInt32, math.MinInt32
+	for index < length && str[index] == ' ' {
+		index += 1
+	}
+
+	if str[index] == '+' {
+		syn = 1
+		index += 1
+	} else if str[index] == '-' {
+		syn = -1
+		index += 1
+	}
+	for index < length && str[index] == '0' {
+		index += 1
+	}
+
+	for index < length && str[index] >= '0' && str[index] <= '9' {
+		res = res*10 + int(str[index]-48)
+		if res*syn < min {
+			return min
+		}
+		if res*syn > max {
+			return max
+		}
+		index += 1
+	}
+
+	return res * syn
+}
