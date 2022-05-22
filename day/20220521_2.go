ee3123a9d5606abe9d0404fb03cb6bc5e4710d2e
lack <598223084@qq.com>
2022-05-22 14:02:45 +0800

    20220522

--- day/20220521_2.go
+++ day/20220521_2.go
@@ -1,22 +1,40 @@
 package day
 
+import "sort"
+
 func threeSum(nums []int) [][]int {
-	out := make([][]int, 0)
+	n := len(nums)
+	sort.Ints(nums)
+	ans := make([][]int, 0)
 
-	length := len(nums)
-	if length < 3 {
-		return out
-	}
-
-	for i := 0; i < length-2; i++ {
-		for j := i + 1; j < length-1; j++ {
-			for k := j + 1; k < length; k++ {
-				if nums[i]+nums[j]+nums[k] == 0 {
-					out = append(out, []int{nums[i], nums[j], nums[k]})
-				}
+	// 枚举 a
+	for first := 0; first < n; first++ {
+		// 需要和上一次枚举的数不相同
+		if first > 0 && nums[first] == nums[first-1] {
+			continue
+		}
+		// c 对应的指针初始指向数组的最右端
+		third := n - 1
+		target := -1 * nums[first]
+		// 枚举 b
+		for second := first + 1; second < n; second++ {
+			// 需要和上一次枚举的数不相同
+			if second > first+1 && nums[second] == nums[second-1] {
+				continue
+			}
+			// 需要保证 b 的指针在 c 的指针的左侧
+			for second < third && nums[second]+nums[third] > target {
+				third--
+			}
+			// 如果指针重合，随着 b 后续的增加
+			// 就不会有满足 a+b+c=0 并且 b<c 的 c 了，可以退出循环
+			if second == third {
+				break
+			}
+			if nums[second]+nums[third] == target {
+				ans = append(ans, []int{nums[first], nums[second], nums[third]})
 			}
 		}
 	}
-
-	return out
+	return ans
 }
