c383092187a21e281dd5692757690e0f58cfb405
lack <598223084@qq.com>
2022-05-23 10:06:48 +0800

    20220523

---
+++ day/20220523_2.go
@@ -0,0 +1,18 @@
+package day
+
+func maxArea(height []int) int {
+	area := 0
+	i, j := 0, len(height)-1
+
+	for i < j {
+		if height[i] < height[j] {
+			area = max(area, height[i]*(j-i))
+			i += 1
+		} else {
+			area = max(area, height[j]*(j-i))
+			j -= 1
+		}
+	}
+
+	return area
+}
