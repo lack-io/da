82bd1b2ee2ce0132b2bdce5672810f81703dfb16
lack <598223084@qq.com>
2022-06-15 23:23:29 +0800

    20220615

--- day/202206/14_01.go
+++ day/202206/14_01.go
@@ -24,17 +24,3 @@ func findDiagonalOrder(mat [][]int) []int {
 	}
 	return ans
 }
-
-func min(a, b int) int {
-	if a > b {
-		return b
-	}
-	return a
-}
-
-func max(a, b int) int {
-	if b > a {
-		return b
-	}
-	return a
-}
---
+++ day/202206/15_01.go
@@ -0,0 +1,15 @@
+package _02206
+
+import "sort"
+
+func smallestDistancePair(nums []int, k int) int {
+	sort.Ints(nums)
+	return sort.Search(nums[len(nums)-1]-nums[0], func(mid int) bool {
+		cnt := 0
+		for j, num := range nums {
+			i := sort.SearchInts(nums[:j], num-mid)
+			cnt += j - i
+		}
+		return cnt >= k
+	})
+}
