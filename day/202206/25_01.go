8210e558f8fcc0c08f93fd763d3a1eafcacd93ab
lack <598223084@qq.com>
2022-06-25 22:50:35 +0800

    20220625

---
+++ day/202206/25_01.go
@@ -0,0 +1,13 @@
+package _02206
+
+func minCost(costs [][]int) int {
+	dp := costs[0]
+	for _, cost := range costs[1:] {
+		dpNew := make([]int, 3)
+		for j, c := range cost {
+			dpNew[j] = min(dp[(j+1)%3], dp[(j+2)%3]) + c
+		}
+		dp = dpNew
+	}
+	return min(min(dp[0], dp[1]), dp[2])
+}
